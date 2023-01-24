e5b6cca97c4d6b019bf8716b2db3e53a6c5d7a1c
Olha Chebotarova <chebotarovaolha@gmail.com>
2023-01-24 15:45:03 -0500

    New fetures add

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ const body = document.querySelector('.page__body');
 const menu = document.getElementById('menu-open');
 const closeMenu = document.getElementById('menu-close');
 const form = document.getElementById('form');
+const menuList = document.querySelector('.menu__list');
 
 menu.addEventListener('click', () => {
   body.classList.add('page__body--no-scroll');
@@ -11,6 +12,12 @@ menu.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
   body.classList.remove('page__body--no-scroll');
+});
+
+menuList.addEventListener('click', (e) => {
+  if (e.target.closest('.menu__link')) {
+    body.classList.remove('page__body--no-scroll');
+  }
 });
 
 form.addEventListener('submit', (e) => {
